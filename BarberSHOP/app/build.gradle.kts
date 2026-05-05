e2fd33dcbf4b8dc44f5276c7a63f28451394725c
MAR0CEMPIRE <portafoliosabdesebbah@gmail.com>
2026-05-05 16:19:39 +0200

    ADADAADA

--- BarberSHOP/app/build.gradle.kts
+++ BarberSHOP/app/build.gradle.kts
@@ -51,13 +51,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.runtime)
-
-    // View-based UI
     implementation(libs.androidx.constraintlayout)
     implementation(libs.material)
     implementation(libs.navigation.fragment.ktx)
     implementation(libs.navigation.ui.ktx)
-
+    implementation("de.hdodenhof:circleimageview:3.1.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
